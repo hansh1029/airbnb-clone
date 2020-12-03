d835bc47bed52affe4990fead27b4a9ce39b0076
hansh1029 <hansh1029@gmail.com>
2020-12-03 12:03:11 +0900

    favlist save, template tage

--- users/models.py
+++ users/models.py
@@ -59,7 +59,7 @@ class User(AbstractUser):
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=CURRENCY_KRW
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
     )
     currency = models.CharField(
         choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
